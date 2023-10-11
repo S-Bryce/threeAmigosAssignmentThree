0b19d6b00132a9bc09325826c59bd0d287c5a3f4
Bryce Shurts <bshurts@smu.edu>
2023-10-11 17:00:56 -0500

    Initial Commit

---
+++ threeAmigosAssignmentThree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  threeAmigosAssignmentThree
+//
+//  Created by Bryce Shurts on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
